bd363842bb49facd95b720661e479861b128ac9e
jgrimTT <jgrim@tenstorrent.com>
2025-07-16 01:29:10 +0000

    Save

---
+++ python/ttmlir/dialects/sdy.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: (c) 2024 Tenstorrent AI ULC
+#
+# SPDX-License-Identifier: Apache-2.0
+
+from ._sdy_enums_gen import *
+from ._sdy_ops_gen import *
+from .._mlir_libs._sdy import *
